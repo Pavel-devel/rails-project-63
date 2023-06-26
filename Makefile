259e5e1d56a35098f4825bf2243b4c2eee415b59
Pavel-Devel <fominlzpv@gmail.com>
2023-06-26 13:24:30 +0300

    git_actions

--- Makefile
+++ Makefile
@@ -1,2 +1,11 @@
-test:
+install:
 	bundle install
+
+lint:
+	bundle exec rubocop
+
+lint-fix:
+	bundle exec rubocop -A
+
+test:
+    bundle exec rake rspec
--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-require_relative "hexlet_code/version"
+require_relative 'hexlet_code/version'
 
 module HexletCode
   class Error < StandardError; end
 
+  # module HexletCode::Tag
   module Tag
-    def self.build(tag_name, attributes = {}, &block)
-      attributes_str = attributes.map { |attr, value| "#{attr}=\"#{value}\"" }.join(" ")
+    def self.build(tag_name, attributes = {}, &_block)
+      attributes_str = attributes.map { |attr, value| "#{attr}=\"#{value}\"" }.join(' ')
       content = block_given? ? yield : ''
       if content.empty?
         "<#{tag_name} #{attributes_str}>"
