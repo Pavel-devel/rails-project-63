b89463ddde97066c9822d3247c2f3a18cac4aa66
Pavel-Devel <fominlzpv@gmail.com>
2023-06-26 10:17:13 +0300

    tag_generation

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -4,5 +4,16 @@ require_relative "hexlet_code/version"
 
 module HexletCode
   class Error < StandardError; end
-  # Your code goes here...
+
+  module Tag
+    def self.build(tag_name, attributes = {}, &block)
+      attributes_str = attributes.map { |attr, value| "#{attr}=\"#{value}\"" }.join(" ")
+      content = block_given? ? yield : ''
+      if content.empty?
+        "<#{tag_name} #{attributes_str}>"
+      else
+        "<#{tag_name} #{attributes_str}>#{content}</#{tag_name}>"
+      end
+    end
+  end
 end
